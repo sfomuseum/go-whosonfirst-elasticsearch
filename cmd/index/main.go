56d1e28bc77ad3c405f389d3e252691c4aad7641
thisisaaronland <thisisaaronland@localhost>
2020-06-04 23:07:30 +0000

    snapshot w/ working bulk code, untested

--- cmd/index/main.go
+++ cmd/index/main.go
@@ -5,18 +5,23 @@ import (
 )
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"flag"
 	"fmt"
+	"github.com/cenkalti/backoff/v4"
 	es "github.com/elastic/go-elasticsearch/v7"
-	esapi "github.com/elastic/go-elasticsearch/v7/esapi"
-	"github.com/whosonfirst/go-whosonfirst-index"
+	_ "github.com/elastic/go-elasticsearch/v7/esapi"
+	"github.com/elastic/go-elasticsearch/v7/esutil"
 	"github.com/tidwall/gjson"
+	"github.com/whosonfirst/go-whosonfirst-index"
 	"io"
 	"io/ioutil"
 	"log"
+	"runtime"
 	"strconv"
+	"time"
 )
 
 func main() {
@@ -26,17 +31,27 @@ func main() {
 
 	idx_uri := flag.String("indexer-uri", "repo://", "...")
 
+	workers := flag.Int("workers", runtime.NumCPU(), "...")
+	flush_bytes := flag.Int("flush", 5e+6, "Flush threshold in bytes")
+
 	// bulk := flag.Bool("bulk", false, "...")
 
 	flag.Parse()
 
 	ctx := context.Background()
 
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
+	retryBackoff := backoff.NewExponentialBackOff()
 
 	es_cfg := es.Config{
-		Addresses: []string{*es_endpoint},
+		Addresses:     []string{*es_endpoint},
+		RetryOnStatus: []int{502, 503, 504, 429},
+		RetryBackoff: func(i int) time.Duration {
+			if i == 1 {
+				retryBackoff.Reset()
+			}
+			return retryBackoff.NextBackOff()
+		},
+		MaxRetries: 5,
 	}
 
 	es_client, err := es.NewClient(es_cfg)
@@ -51,29 +66,24 @@ func main() {
 		log.Fatalf("Failed to create ES index, %v", err)
 	}
 
-	cb := func(ctx context.Context, fh io.Reader, args ...interface{}) error {
+	// https://github.com/elastic/go-elasticsearch/blob/master/_examples/bulk/indexer.go
 
-		path, err := index.PathForContext(ctx)
+	bi, err := esutil.NewBulkIndexer(esutil.BulkIndexerConfig{
+		Index:         *es_index,
+		Client:        es_client,
+		NumWorkers:    *workers,
+		FlushBytes:    int(*flush_bytes),
+		FlushInterval: 30 * time.Second,
+	})
 
-		if err != nil {
-			return err
-		}
+	if err != nil {
+		log.Fatalf("Error creating the indexer: %s", err)
+	}
 
-		body, err := ioutil.ReadAll(fh)
+	defer bi.Close(ctx)
 
-		if err != nil {
-		return err
-		}		
-
-		id_rsp := gjson.GetBytes(body, "properties.wof:id")
-
-		if !id_rsp.Exists(){
-		msg := fmt.Sprintf("%s is missing properties.wof:id", path)
-		   return errors.New(msg)
-		}
-
-		wof_id := id_rsp.Int()
-		doc_id := strconv.FormatInt(wof_id, 10)
+	/*
+	index_record := func(ctx context.Context, doc_id string, fh io.Reader) error {
 
 		req := esapi.IndexRequest{
 			Index:      *es_index,
@@ -90,14 +100,65 @@ func main() {
 
 		defer rsp.Body.Close()
 
-		switch rsp.StatusCode {
-		case 200, 201:
-		// pass
-		default:
+		if rsp.IsError() {
 			body, _ := ioutil.ReadAll(rsp.Body)
-			// return errors.New(string(body))
+			return errors.New(string(body))
+		}
 
-			log.Println(string(body))
+		return nil
+	}
+	*/
+
+	cb := func(ctx context.Context, fh io.Reader, args ...interface{}) error {
+
+		path, err := index.PathForContext(ctx)
+
+		if err != nil {
+			return err
+		}
+
+		body, err := ioutil.ReadAll(fh)
+
+		if err != nil {
+			return err
+		}
+
+		id_rsp := gjson.GetBytes(body, "properties.wof:id")
+
+		if !id_rsp.Exists() {
+			msg := fmt.Sprintf("%s is missing properties.wof:id", path)
+			return errors.New(msg)
+		}
+
+		wof_id := id_rsp.Int()
+		doc_id := strconv.FormatInt(wof_id, 10)
+
+		// manipulate body here...
+
+		bulk_item := esutil.BulkIndexerItem{
+			Action:     "index",
+			DocumentID: doc_id,
+			Body:       bytes.NewReader(body),
+
+			OnSuccess: func(ctx context.Context, item esutil.BulkIndexerItem, res esutil.BulkIndexerResponseItem) {
+				log.Printf("Indexed %s\n", path)
+			},
+
+			OnFailure: func(ctx context.Context, item esutil.BulkIndexerItem, res esutil.BulkIndexerResponseItem, err error) {
+				if err != nil {
+					log.Printf("ERROR: Failed to index %s, %s", path, err)
+				} else {
+					log.Printf("ERROR: Failed to index %s, %s: %s", path, res.Error.Type, res.Error.Reason)
+				}
+			},
+		}
+
+		err = bi.Add(ctx, bulk_item)
+
+		// err = index_record(ctx, doc_id, br)
+
+		if err != nil {
+			log.Printf("Failed to index %s, %v", path, err)
 		}
 
 		return nil
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/sfomuseum/go-sfomuseum-elasticsearch-index
 go 1.12
 
 require (
+	github.com/cenkalti/backoff/v4 v4.0.2
 	github.com/elastic/go-elasticsearch/v7 v7.7.0
 	github.com/tidwall/gjson v1.6.0
 	github.com/whosonfirst/go-whosonfirst-index v0.3.0
